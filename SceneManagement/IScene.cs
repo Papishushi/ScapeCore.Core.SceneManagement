5efcd0db471391267e4407b221535eb0579f6932
Papishushi <danielmolinero111@cyberdude.com>
2024-03-22 16:31:45 +0100

    Updated SceneManagement to use dependency injection

---
+++ SceneManagement/IScene.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * -*- encoding: utf-8 with BOM -*-
+ * .▄▄ ·  ▄▄·  ▄▄▄·  ▄▄▄·▄▄▄ .     ▄▄·       ▄▄▄  ▄▄▄ .
+ * ▐█ ▀. ▐█ ▌▪▐█ ▀█ ▐█ ▄█▀▄.▀·    ▐█ ▌▪▪     ▀▄ █·▀▄.▀·
+ * ▄▀▀▀█▄██ ▄▄▄█▀▀█  ██▀·▐▀▀▪▄    ██ ▄▄ ▄█▀▄ ▐▀▀▄ ▐▀▀▪▄
+ * ▐█▄▪▐█▐███▌▐█ ▪▐▌▐█▪·•▐█▄▄▌    ▐███▌▐█▌.▐▌▐█•█▌▐█▄▄▌
+ *  ▀▀▀▀ ·▀▀▀  ▀  ▀ .▀    ▀▀▀     ·▀▀▀  ▀█▄▀▪.▀  ▀ ▀▀▀ 
+ * https://github.com/Papishushi/ScapeCore
+ * 
+ * Copyright (c) 2023 Daniel Molinero Lucas
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ * 
+ * Scene.cs
+ * Represents an environment containing a collection of active behaviours, exposes
+ * multiple methods to manipulate the scene. This class is mainly used in the
+ * Sceme Management system.
+ */
+
+using ScapeCore.Core.Tools;
+using ScapeCore.Core.Engine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ScapeCore.Core.SceneManagement
+{
+    public interface IScene : IDisposable, IScapeCoreService
+    {
+        string Name { get; set; }
+
+        IList GameObjects { get; }
+        IList MonoBehaviours { get; }
+
+        DeeplyMutableType AddToScene(Type type);
+        T? AddToScene<T>() where T : MonoBehaviour;
+        Task<DeeplyMutableType> AddToSceneAsync(Type type);
+        Task<T?> AddToSceneAsync<T>() where T : MonoBehaviour;
+        Task<List<T>> AddToSceneMultipleAsync<T>(int cuantity) where T : MonoBehaviour;
+        void Dispose();
+        IEnumerator<GameObject>? FindGameObjectsWithTag(string tag);
+        GameObject? FindGameObjectWithTag(string tag);
+        void RemoveFromScene(GameObject gameObject);
+        void RemoveFromScene(MonoBehaviour monoBehaviour);
+    }
+}
--- SceneManagement/SceneManager.cs
+++ SceneManagement/SceneManager.cs
@@ -19,6 +19,10 @@
  * scenes within the application.
  */
 
+using ProtoBuf.Meta;
+using ScapeCore.Core.Serialization.Streamers;
+using ScapeCore.Core.Serialization;
+using ScapeCore.Core.Tools;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
@@ -26,21 +30,30 @@ using System.Collections.Immutable;
 using System.Linq;
 using System.Runtime.InteropServices;
 
-using static ScapeCore.Traceability.Debug.Debugger;
+using static ScapeCore.Core.Debug.Debugger;
 
 namespace ScapeCore.Core.SceneManagement
 {
-    public static class SceneManager
+    public class SceneManager : IScapeCoreManager
     {
-        private static readonly ConcurrentDictionary<int, Scene> _scenes = new();
-        private static int _scenesCount = 0;
-        private static int _currentSceneIndex = 0;
-        public static WeakReference<Scene?> CurrentScene { get => new(_scenes.GetValueOrDefault(_currentSceneIndex)); }
-        public static ImmutableList<Scene> Scenes { get => _scenes.Values.ToImmutableList(); }
-        public static int Count { get => _scenesCount; }
+        private readonly ConcurrentDictionary<int, IScene> _scenes = new();
+        private int _scenesCount = 0;
+        private int _currentSceneIndex = 0;
+        public WeakReference<IScene?> CurrentScene { get => new(_scenes.GetValueOrDefault(_currentSceneIndex)); }
+        public ImmutableList<IScene> Scenes { get => _scenes.Values.ToImmutableList(); }
+        public int Count { get => _scenesCount; }
+        List<IScapeCoreService?> IScapeCoreManager.Services { get => [.. _scenes.Values]; }
+        private static SceneManager? _defaultManager = null;
 
-        public static void SetCurrentScene(int sceneIndex) => _currentSceneIndex = sceneIndex;
-        public static Scene? Get(int sceneId)
+        public static SceneManager? Default { get => _defaultManager; set => _defaultManager = value; }
+
+        public SceneManager()
+        {
+            _defaultManager ??= this;
+        }
+
+        public void SetCurrentScene(int sceneIndex) => _currentSceneIndex = sceneIndex;
+        public IScene? Get(int sceneId)
         {
             if (_scenes.TryGetValue(sceneId, out var scene))
                 return scene;
@@ -48,37 +61,50 @@ namespace ScapeCore.Core.SceneManagement
                 SCLog.Log(ERROR, $"Scene with ID {sceneId} not found in the SceneManager");
             return null;
         }
-        public static int AddScene(Scene scene)
+        public int AddScene(IScene scene)
         {
             if (_scenesCount <= 0)
                 _scenes.TryAdd(0, scene);
             else if (!_scenes.TryAdd(_scenes.Last().Key + 1, scene))
             {
-                SCLog.Log(ERROR, $"There was a problem whilst trying to add Scene {scene.name} to the SceneManager");
+                SCLog.Log(ERROR, $"There was a problem whilst trying to add Scene {scene.Name} to the SceneManager");
                 return -1;
             }
             _scenesCount++;
             return _scenes.Last().Key;
         }
-        public static int RemoveScene(int sceneId)
+        public int RemoveScene(int sceneId)
         {
             if (_scenesCount <= 0)
                 return -1;
             if (!_scenes.TryRemove(sceneId, out var scene))
             {
-                SCLog.Log(ERROR, $"There was a problem whilst trying to remove scene {scene?.name} to the SceneManager");
+                SCLog.Log(ERROR, $"There was a problem whilst trying to remove scene {scene?.Name} to the SceneManager");
                 return -1;
             }
             _scenesCount--;
             return sceneId;
         }
-        public static void Clear()
+        public void Clear()
         {
             foreach (var scene in _scenes)
                 scene.Value.Dispose();
             _scenes.Clear();
         }
 
+        bool IScapeCoreManager.InjectDependencies(params IScapeCoreService[] services)
+        {
+            if (services.Length <= 0)
+                return false;
 
+            foreach(var service in services)
+            {
+                if (service is IScene)
+                    AddScene(service as IScene);
+                else return false;
+            }
+
+            return true;
+        }
     }
 }
